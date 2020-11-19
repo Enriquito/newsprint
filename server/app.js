2f2298ac206e4d6569d1deb5b256e98c4710b7ff
Enrique Kreuk <Enrique.Kreuk@zuiderzeemuseum.nl>
2020-11-19 15:54:29 +0100

    created

---
+++ server/app.js
@@ -0,0 +1,33 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+
+// Controllers
+const feedRoutes = require('./controllers/feeds');
+const folderRoutes = require('./controllers/folders');
+
+dotenv.config();
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+
+app.use(function (req, res, next) {
+      res.setHeader('Access-Control-Allow-Origin', `${process.env.ALLOW_ORGIN}`);
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+      res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+      res.setHeader('Access-Control-Allow-Credentials', true);
+next();
+});
+
+app.route('/feeds/:id')
+.get(feedRoutes.findOne)
+
+app.route('/folders/:id')
+.get(folderRoutes.findOne)
+
+app.route('/folders/user/:id')
+.get(folderRoutes.findFoldersByUser)
+
+app.listen(process.env.PORT, () => {
+      console.log(`listening on port ${process.env.PORT}`)
+});
