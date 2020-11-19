1484d75aaf2099fb832aa5abab3b88a65bece828
Enrique Kreuk <Enrique.Kreuk@zuiderzeemuseum.nl>
2020-11-19 10:38:14 +0100

    created

---
+++ server/models/articles.js
@@ -0,0 +1,63 @@
+const database = require('../database');
+
+class Article{
+      constructor(){
+            this.id;
+            this.creator;
+            this.title;
+            this.pubDate;
+            this.content;
+            this.contentSnippet;
+            this.isoDate;
+            this.categories = [];
+      }
+
+      create(){
+            return new Promise((resolve,reject) => {
+                  const toInsert = {
+                        creator: this.creator,
+                        title: this.title,
+                        pubDate: this.pubDate,
+                        content: this.content,
+                        content_snippet: this.contentSnippet,
+                        isoDate: this.isoDate
+                  };
+      
+                  database.query('INSERT articles SET ?', [toInsert], (error, result) => {
+                        if(error)
+                            reject(error);
+                        
+                        this.id = result.insertId
+            
+                        resolve(this);
+                  });
+            });
+      }
+
+      createCategories(){
+            return new Promise((resolve,reject) => {
+                  if(this.categories === 0){
+                        reject(null);
+                        return;
+                  }
+            
+                  const toInsert = [];
+
+                  this.categories.forEach(cat => {
+                        toInsert.push({
+                              article: this.id,
+                              category: cat
+                        });
+                  })
+
+                  database.query('INSERT article_categories SET ?', toInsert, (error, result) => {
+                        if(error)
+                              reject(error);
+            
+                        resolve();
+                  });
+            });
+      }
+}
+
+module.exports = Article;
