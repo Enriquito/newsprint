2a211b52e35a13b1a90e1541ab0af2a73b3e3f79
Enrique <enrique.kreuk@icloud.com>
2020-11-20 21:34:59 +0100

    Added get articles

--- server/controllers/feeds.js
+++ server/controllers/feeds.js
@@ -3,21 +3,22 @@ const Feed = require('../models/feed')
 module.exports.findOne = async (req,res) => {
       try{
           console.log(`GET /feed/${req.params.id}`);
-  
+
       //     const validator = Validator.ValidateID(req.params.id);
-  
+
       //     if(validator.error){
       //         res.status(400).json({error: validator.error.details[0].message});
       //         return;
       //     }
-  
+
           const feed = await Feed.findOne(req.params.id);
-  
+          await feed.getArticles();
+
           if(feed === null){
               res.sendStatus(404);
               return;
           }
-  
+
           res.json(feed);
       }
       catch(error){
