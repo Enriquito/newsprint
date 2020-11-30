3583f643a70c2d037cf5e2104e6d9adf0d69d7e4
Enrique Kreuk <Enrique.Kreuk@zuiderzeemuseum.nl>
2020-11-30 10:37:21 +0100

    added max & offset

--- server/models/feed.js
+++ server/models/feed.js
@@ -98,9 +98,9 @@ class Feed{
             });
       }
 
-      getArticles(){
+      getArticles(max, offset){
             return new Promise( async (resolve, reject) => {
-                  database.query('SELECT * FROM articles WHERE feed = ? ORDER BY DATE(iso_date) DESC', [this.id], async (error, result) => {
+                  database.query('SELECT * FROM articles WHERE feed = ? ORDER BY DATE(iso_date) DESC LIMIT ? OFFSET ?', [this.id, max, offset], async (error, result) => {
                         if(error){
                               console.log(error);
                               reject(error);
