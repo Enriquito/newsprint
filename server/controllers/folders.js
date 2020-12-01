c99d9c637b947784b412939d040717ea90f8d709
Enrique <enrique.kreuk@icloud.com>
2020-12-01 11:41:42 +0100

    Removed request bodt params

--- server/controllers/folders.js
+++ server/controllers/folders.js
@@ -4,23 +4,23 @@ const Folder = require('../models/folder')
 module.exports.findOne = async (req,res) => {
       try{
           console.log(`GET /feed/${req.params.id}`);
-  
+
       //     const validator = Validator.ValidateID(req.params.id);
-  
+
       //     if(validator.error){
       //         res.status(400).json({error: validator.error.details[0].message});
       //         return;
       //     }
-  
+
           const folder = await Folder.findOne(req.params.id);
-  
+
           if(folder === null){
               res.sendStatus(404);
               return;
           }
 
           await folder.getFeeds();
-  
+
           res.json(folder);
       }
       catch(error){
@@ -28,24 +28,25 @@ module.exports.findOne = async (req,res) => {
           res.sendStatus(500);
       }
 }
+// Change user
 module.exports.findFoldersByUser = async (req,res) => {
       try{
           console.log(`GET /folders/user/${req.params.id}`);
-  
+
       //     const validator = Validator.ValidateID(req.params.id);
-  
+
       //     if(validator.error){
       //         res.status(400).json({error: validator.error.details[0].message});
       //         return;
       //     }
-  
-          const folders = await Folder.findAllByUser(req.params.id);
-  
+
+          const folders = await Folder.findAllByUser(1);
+
           if(folders === null){
               res.sendStatus(404);
               return;
           }
-  
+
           res.json(folders);
       }
       catch(error){
