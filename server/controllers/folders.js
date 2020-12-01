5bc648d399226f34e83f78308267ad5cd54e3f53
Enrique <enrique.kreuk@icloud.com>
2020-12-01 16:58:28 +0100

    Added create request

--- server/controllers/folders.js
+++ server/controllers/folders.js
@@ -54,3 +54,30 @@ module.exports.findFoldersByUser = async (req,res) => {
           res.sendStatus(500);
       }
 }
+
+module.exports.create = async (req,res) => {
+    console.log(`POST /folders/`);
+    try{
+
+
+    //     const validator = Validator.ValidateID(req.params.id);
+
+    //     if(validator.error){
+    //         res.status(400).json({error: validator.error.details[0].message});
+    //         return;
+    //     }
+
+        const folder = new Folder();
+
+        folder.name = req.body.name;
+        folder.user = 1;
+
+        await folder.create();
+
+        res.status(201).json(folder);
+    }
+    catch(error){
+        console.log(error);
+        res.sendStatus(500);
+    }
+}
