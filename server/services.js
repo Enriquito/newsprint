5e5abf6f93eaf5753d767a9c6f9261b12565c99c
Enrique <enrique.kreuk@icloud.com>
2020-12-13 20:02:41 +0100

    added functions

--- server/services.js
+++ server/services.js
@@ -1,11 +1,29 @@
 const Feed = require('./models/feed');
+const Article = require('./models/article');
+const cron = require('node-cron');
 
-console.log(`scanning for new articles...`)
 
-Feed.getNewItems()
-.then(result => {
-      console.log('Scan complete.');
-})
-.catch(error => {
-      console.log(error);
-});
+function articleScan(){
+      console.log(`scanning for new articles...`)
+
+      Feed.getNewItems()
+      .then(result => {
+            console.log('Scan complete.');
+      })
+      .catch(error => {
+            console.log(error);
+      });
+}
+function deleteOldArticles(){
+      Article.deleteOldArticles(2)
+      .then(result => {
+            console.log(`Rows deleted: ${result.affectedRows}`)
+      })
+      .catch(error => {
+            console.log(error);
+      });
+}
+
+// Every hour
+cron.schedule('0 * * * *', articleScan);
+cron.schedule('0 * * * *', deleteOldArticles);
