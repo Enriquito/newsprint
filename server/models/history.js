c88eabd8d4d130e57513cc51e9b88c8438c5be93
Enrique <enrique.kreuk@icloud.com>
2020-12-13 20:02:23 +0100

    Changed query

--- server/models/history.js
+++ server/models/history.js
@@ -20,7 +20,22 @@ class History{
 
     static getAll(userId){
         return new Promise((resolve, reject) => {
-            database.query(`SELECT article FROM article_history WHERE user = ? ORDER BY created DESC`, [userId], async (error, result) => {
+            const query = `SELECT
+            a.id, a.feed, a.title, a.link, a.pub_date, a.content, a.content_snippet, a.iso_date, a.is_read, fe.icon_url,
+            CASE WHEN fe.display_name IS NOT NULL THEN fe.display_name ELSE fe.title END as 'display_name'
+            FROM articles a
+            LEFT JOIN favorites f
+            ON a.id = f.article
+            LEFT JOIN users u
+            ON u.id = f.user
+            LEFT JOIN feeds fe
+            ON fe.id = a.feed
+            JOIN article_history ah
+            ON ah.article = a.id
+            WHERE fe.user = ${userId}
+            ORDER BY ah.created DESC`;
+
+            database.query(query, [userId], async (error, result) => {
                 if(error){
                       reject(error);
                       return;
@@ -31,11 +46,35 @@ class History{
                     return;
                 }
 
+                const Feed = require('./feed');
                 const articles = [];
 
                 if(result.length > 0){
                     for(let i = 0; i < result.length; i++){
-                        const article = await Article.findOne(result[i].article);
+                        const f = result[i];
+
+                        const article = new Article();
+                        const feed = new Feed();
+
+                        feed.iconUrl = f.icon_url;
+                        feed.displayName = f.display_name;
+                        feed.id = f.feed;
+
+                        article.id = f.id;
+                        article.creator = f.creator;
+                        article.title = f.title;
+                        article.link = f.link;
+                        article.pubDate = f.pub_date;
+                        article.content = f.content;
+                        article.contentSnippet = f.content_snippet;
+                        article.isoDate = f.iso_date
+                        article.favorite = f.favorite;
+                        article.feed = feed;
+
+                        if(f.is_read == 1)
+                              article.isRead = true;
+                        else
+                              article.isRead = false;
 
                         if(article !== null){
                             articles.push(article);
