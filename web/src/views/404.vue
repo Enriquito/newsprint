4717da78a48502559854fc0b71e40d7f3768a3f2
Enrique <enrique.kreuk@icloud.com>
2020-12-14 10:40:36 +0100

    created

---
+++ web/src/views/404.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="content-wrapper">
+    <MobileMenuIcon />
+    <Navigation />
+    <section>
+        <h1>Page not found</h1>
+        <h5>This page can not be found.</h5>
+    </section>
+  </div>
+</template>
+<script>
+import MobileMenuIcon from '@/components/MobileMenuIcon.vue';
+import Navigation from '@/components/Navigation.vue';
+
+export default {
+    name: "NotFound",
+    components: {
+          MobileMenuIcon,
+          Navigation
+    }
+}
+</script>
