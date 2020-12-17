dd9480c59ee48d0ef0d922a295f4fbb06a55818d
Enrique <enrique.kreuk@icloud.com>
2020-12-17 21:26:47 +0100

    created

---
+++ web/src/views/500.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="content-wrapper">
+    <MobileMenuIcon />
+    <Navigation />
+    <section>
+        <h1>Internal server error</h1>
+        <h5>Well, that did not go wel.</h5>
+        <h5>Go back to home. Or try again later.</h5>
+    </section>
+  </div>
+</template>
+<script>
+import MobileMenuIcon from '@/components/MobileMenuIcon.vue';
+import Navigation from '@/components/Navigation.vue';
+
+export default {
+    name: "ServerError",
+    components: {
+          MobileMenuIcon,
+          Navigation
+    }
+}
+</script>
