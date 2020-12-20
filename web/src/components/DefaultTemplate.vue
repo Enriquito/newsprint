9dfb5990dc47d9ccff1cab5563289d59f45338f0
Enrique Kreuk <enrique.kreuk@icloud.com>
2020-12-20 19:43:36 +0100

    created

---
+++ web/src/components/DefaultTemplate.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="d-flex content-wrapper">
+    <MobileMenuIcon />
+    <Navigation />
+    <slot>
+        
+    </slot>
+  </div>
+</template>
+<script>
+import MobileMenuIcon from '@/components/MobileMenuIcon.vue';
+import Navigation from '@/components/Navigation.vue';
+
+export default {
+    name: "DefaultTemplate",
+    components: {
+          MobileMenuIcon,
+          Navigation
+    }
+}
+</script>
