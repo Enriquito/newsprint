577aea03a0b77c44657b42379f22e01456fae3bc
Enrique Kreuk <enrique.kreuk@icloud.com>
2020-12-23 15:53:07 +0100

    fixed typo

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,4 +1,4 @@
-const User = require('../models/User');
+const User = require('../models/user');
 const Validator = require('../validators');
 
 module.exports.login = (username, password, done) => {
