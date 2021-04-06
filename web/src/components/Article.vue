28793bd738332b642e61fc978641444ae85128f5
Enrique Kreuk <enrique.kreuk@icloud.com>
2021-04-06 21:14:19 +0200

    Changed date data from iso to pub date

--- web/src/components/Article.vue
+++ web/src/components/Article.vue
@@ -186,7 +186,7 @@ export default {
     computed:{
       date(){
         if(moment(this.data.isoDate).format('D-MM-YYYY') === moment().format('D-MM-YYYY'))
-          return `Today ${moment(this.data.isoDate).format('HH:mm')}`
+          return `Today ${moment(this.data.pubDate).utcOffset(moment.tz.guess()).format('HH:mm')}`
         else
           return moment(this.data.isoDate).format('LL')
       },
